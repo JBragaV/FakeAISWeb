7ab910dcdd6dceeee0d0ed3c3169b6b1fe712afa
JBragaV <jocimarcaiadobraga@gmail.com>
2026-05-22 11:31:41 -0300

    Inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+          <h1>Hello, World</h1>
           <p>
-            Looking for a starting point or more instructions? Head over to{" "}
+            It is a little app with next-js? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
